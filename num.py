b47132fd39fa69d079555bc4a9808519da1ccd10
Pj <praveenjayakar7@gmail.com>
2023-12-17 04:43:28 +0000

    added files

---
+++ num.py
@@ -0,0 +1,6 @@
+num1 = 20
+num2 = 25
+
+sum = num1 + num2
+
+print(f"total sum of total : {sum}")
---
+++ string-reverse.py
@@ -0,0 +1,8 @@
+mystr = "praveen"
+
+rev = mystr[::-1]
+rev1 = mystr[::-2]
+
+print(rev)
+print(rev1)
+
